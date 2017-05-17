84c11edd9f789ac81b48dcf1f31a4fa93f143a86
maticaputti <matias.caputti@gmail.com>
2017-05-17 17:17:24 -0300

    Add TODO example

--- main.js
+++ main.js
@@ -1,12 +1,82 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text, View, TextInput,
+  Button, StatusBar, StyleSheet,
+} from 'react-native';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newTodo: '',
+      todos: [],
+    };
+
+    this.handleChangeText = this.handleChangeText.bind(this);
+    this.handlePress = this.handlePress.bind(this);
+    this.renderTodos = this.renderTodos.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  handleChangeText(text) {
+    this.setState({
+      newTodo: text,
+    });
+  }
+
+  handlePress() {
+    const { newTodo, todos } = this.state;
+
+    const updatedTodos = todos.concat([newTodo]);
+
+    this.setState({
+      todos: updatedTodos,
+      newTodo: '',
+    });
+  }
+
+  renderTodos() {
+    const { todos } = this.state;
+
+    return todos.map((todo, index) => (
+      <Text
+        key={todo + index}
+        style={styles.todo}
+      >
+        {todo}
+      </Text>
+    ));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Text style={styles.title}>
+          FIREBASE TO DO LIST
+        </Text>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.handleChangeText(text)}
+            value={this.state.newTodo}
+            placeholder={'Enter a new TO DO'}
+          />
+        </View>
+
+        <Button
+          title={'Add TO DO'}
+          onPress={this.handlePress}
+        />
+
+        {this.renderTodos()}
+
+        <StatusBar barStyle={'dark-content'} />
       </View>
     );
   }
@@ -15,9 +85,32 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    paddingTop: 40,
+  },
+  title: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: '900',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  inputContainer: {
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    width: 300,
+    borderWidth: 2,
+    borderColor: '#ddd',
+    margin: 10,
+    padding: 15,
+  },
+  todo: {
+    color: '#444',
+    fontSize: 16,
+    margin: 2,
   },
 });
 
